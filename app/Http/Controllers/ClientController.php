ff79595c322701099ddab31cf2fbb6e945202941
Samuel Terra <samuelterra22@hotmail.com>
2017-03-01 15:42:28 -0300

    Commit initial

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Models\Client;
+use Illuminate\Http\Request;
+
+/**
+ * Class ClientController
+ *
+ * @package CodeProject\Http\Controllers
+ */
+class ClientController extends Controller
+{
+
+
+    public function index(){
+        return Client::all();
+    }
+
+    public function store(Request $request){
+        return Client::create($request->all());
+    }
+
+    public function show($id){
+        return Client::find($id);
+    }
+
+    public function destroy($id){
+        Client::find($id)->delete();
+    }
+
+    public function update(Request $request, $id){
+
+    }
+
+}
---
+++ database/seeds/ClientTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+
+use CodeProject\Models\Client;
+use Illuminate\Database\Seeder;
+
+class ClientTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Client::truncate();
+        factory(Client::class, 10)->create();
+    }
+}
